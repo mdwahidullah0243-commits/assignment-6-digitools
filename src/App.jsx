3ef8f5c6ea6092182e361f25c626fb9c592bb3d0
mdwahidullah0243 <mdwahidullah0243@gmail.com>
2026-03-30 01:26:18 +0600

    Initialized the React project with all resources

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-semibold text-red-700 bg-black text-center py-8">React Project Initialized</h1>
+    </>
+  )
+}
+
+export default App
